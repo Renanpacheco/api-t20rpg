9661ee29629e8df3ba51381eada46dc300a39bc6
Renan Bitencourt Pacheco <prenan86@gmail.com>
2023-11-27 18:57:27 -0300

    Add a put, atualization of delete and routes
    Atualization of Delete without a name inside the function, passed on body of requisition, but keeps in loop.
    Atualization of routes, with similar paths.
    Addittion of a Put, for edits characters sheets by id, but nos working.

--- controllers/control.js
+++ controllers/control.js
@@ -1,16 +1,16 @@
-const { ObjectId } = require('mongodb');
-let Post = require('../models/post')
+//const { ObjectId } = require('mongodb');
+const Post = require('../models/post')
+//let character =
 
-
-exports.getControl = (req, res) => {
-    const posts = Post.find().select("_id name")
+exports.getControl = (req, res) => {  // takes all characters
+    const posts = Post.find().select("_id name charisma")
     .then(posts => {
         res. json({posts})
     })
     .catch(err=>console.log(err))
 };
 
-exports.createSheet = (req, res) => {
+exports.createSheet = (req, res) => { // create a character sheet
     const post = new Post(req.body)
     console.log("creating Post: ", req.body)
     post.save()
@@ -19,16 +19,29 @@ exports.createSheet = (req, res) => {
     })
 };
 
-exports.delete = async(req, res) => {
+exports.delete = async(req, res) => { // delete a character by name on body
     try{
         //const aux = await Post.find
-        await Post.deleteOne({"_id" : ObjectId("6554d909c2d1730a56d1b3f3")})
-        
+        //await Post.deleteOne({ name : 'Bun'})
+        await Post.deleteOne(req.body) 
+        //Post.deteleOne(req.params.id)
+        //Post.deleteOne(req.body)       
 
     }catch(err){
         res.json(err)
 
-    }
+    }    
     
-    
+};
+
+exports.updateSheet = (req, res) =>{ //edit a character sheet by id
+    const characterName = {name: 'Monk'}
+    const update = req.body
+    console.log(update)
+
+    const doc = Post.findOneAndUpdate(characterName, update, {new: true});
+    //doc.save()
+    res.status(200).json({
+        post: "Sucess"
+    })
 };
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,11 @@ const controller = require('../controllers/control')
 const router = express.Router()
 
 
-router.get("/", controller.getControl)
-router.post("/create_sheet", controller.createSheet)
-router.delete("/delete", controller.delete)
-
+router.get("/", controller.getControl) // takes all characters
+//router.get("/character/:id") //takes a character with the id
+router.post("/character", controller.createSheet) // create a character sheet
+router.delete("/character", controller.delete) // delete a character by name on body
+router.put("/chearacter/:id", controller.updateSheet) //edit a character sheet by id
+//router.delete("/character/:id", controller.delete) // delete a character by id
 
 module.exports = router
